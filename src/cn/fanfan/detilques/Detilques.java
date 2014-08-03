d674daa1f3cf65499070cbac52d4bcba50fd3f0c
hugetimor <hugetimor@gamil.com>
2014-08-03 13:46:24 +0800

    无

--- src/cn/fanfan/detilques/Detilques.java
+++ src/cn/fanfan/detilques/Detilques.java
@@ -277,19 +277,19 @@ public class Detilques extends Activity {
 		// title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
 		oks.setTitle(question_content);
 		// titleUrl是标题的网络链接，仅在人人网和QQ空间使用
-		oks.setTitleUrl(Config.getValue("ShareQuestionUrl" + question_id));
+		oks.setTitleUrl(Config.getValue("ShareQuestionUrl" )+ question_id);
 		// text是分享文本，所有平台都需要这个字段
 		oks.setText(question_content+"  ");
 		// imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
 		oks.setImagePath("/sdcard/test.jpg");
 		// url仅在微信（包括好友和朋友圈）中使用
-		oks.setUrl(Config.getValue("ShareQuestionUrl" + question_id));
+		oks.setUrl(Config.getValue("ShareQuestionUrl" )+ question_id);
 		// comment是我对这条分享的评论，仅在人人网和QQ空间使用
 		oks.setComment("  ");
 		// site是分享此内容的网站名称，仅在QQ空间使用
 		oks.setSite(getString(R.string.app_name));
 		// siteUrl是分享此内容的网站地址，仅在QQ空间使用
-		oks.setSiteUrl(Config.getValue("ShareQuestionUrl" + question_id));
+		oks.setSiteUrl(Config.getValue("ShareQuestionUrl" )+ question_id);
 
 		// 启动分享GUI
 		oks.show(this);
