bfd8a9dc5e0e6810ea0e816f81dcabe311769bf3
hugetimor <hugetimor@gamil.com>
2014-08-05 00:40:35 +0800

    201408050040 美化分享界面和结果

--- src/cn/fanfan/detilques/Detilques.java
+++ src/cn/fanfan/detilques/Detilques.java
@@ -1,7 +1,6 @@
 package cn.fanfan.detilques;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.apache.http.Header;
@@ -13,11 +12,8 @@ import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import cn.fanfan.common.Config;
 import cn.fanfan.common.GetUserNamImage;
-import cn.fanfan.common.GlobalVariables;
 import cn.fanfan.common.TextShow;
 import cn.fanfan.main.R;
-import cn.fanfan.userinfo.UserInfoActivity;
-import cn.fanfan.userinfo.UserInfoEditActivity;
 import cn.sharesdk.framework.ShareSDK;
 import cn.sharesdk.onekeyshare.OnekeyShare;
 import android.app.Activity;
@@ -293,6 +289,8 @@ public class Detilques extends Activity {
 		// 分享时Notification的图标和文字
 		oks.setNotification(R.drawable.ic_launcher,
 				getString(R.string.app_name));
+		oks.setDialogMode();// 设置成窗口模式
+		oks.setImageUrl(Config.getValue("IconUrl"));
 		// title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
 		oks.setTitle(question_content);
 		// titleUrl是标题的网络链接，仅在人人网和QQ空间使用
@@ -303,7 +301,8 @@ public class Detilques extends Activity {
 		oks.setText(question_content + Config.getValue("ShareQuestionUrl")
 				+ question_id + "（分享自饭饭安卓端）");
 		// imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-		oks.setImagePath("/sdcard/test.jpg");
+
+		// oks.setImagePath("/sdcard/test.jpg");// 本地图片到时候添加
 		// url仅在微信（包括好友和朋友圈）中使用
 		oks.setUrl(Config.getValue("ShareQuestionUrl") + question_id);
 		// comment是我对这条分享的评论，仅在人人网和QQ空间使用
@@ -312,7 +311,6 @@ public class Detilques extends Activity {
 		oks.setSite(getString(R.string.app_name));
 		// siteUrl是分享此内容的网站地址，仅在QQ空间使用
 		oks.setSiteUrl(Config.getValue("ShareQuestionUrl") + question_id);
-
 		// 启动分享GUI
 		oks.show(this);
 	}
