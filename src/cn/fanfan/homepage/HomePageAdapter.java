b53ecc8a391064d375323a0dcad2d7eff80a8f65
hugetimor <hugetimor@gamil.com>
2014-08-10 14:58:45 +0800

    201808101458增加了首页模块，待添加上拉刷新下拉加载更多和加载头像

--- src/cn/fanfan/homepage/HomePageAdapter.java
+++ src/cn/fanfan/homepage/HomePageAdapter.java
@@ -2,27 +2,40 @@ package cn.fanfan.homepage;
 
 import java.util.List;
 
+import cn.fanfan.common.AsyncImageGet;
+import cn.fanfan.main.MainActivity;
 import cn.fanfan.main.R;
+import cn.fanfan.topic.imageload.ImageDownLoader;
+import cn.fanfan.userinfo.UserInfoActivity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.sax.StartElementListener;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomePageAdapter extends ArrayAdapter<HomePageItemModel> {
 	private int resourceId;
+	private Context context;
 
 	public HomePageAdapter(Context context, int itemViewResourceId,
 			List<HomePageItemModel> objects) {
 		super(context, itemViewResourceId, objects);
+		this.context = context;
 		resourceId = itemViewResourceId;
+
 	}
 
 	public View getView(int position, View convertView, ViewGroup parent) {
-		HomePageItemModel itemModel = getItem(position);
+		final HomePageItemModel itemModel = getItem(position);
 		// 优化ListView性能
 		View view;
 		ViewHolder viewHolder;
@@ -30,13 +43,19 @@ public class HomePageAdapter extends ArrayAdapter<HomePageItemModel> {
 			view = LayoutInflater.from(getContext()).inflate(resourceId, null);
 			viewHolder = new ViewHolder();
 			viewHolder.complexLayout = (LinearLayout) view
-					.findViewById(R.id.llsHomeListItemContent);
+					.findViewById(R.id.llHomeListItemContent);
+			if (itemModel.getLayoutType() == HomePageItemModel.LAYOUT_TYPE_SIMPLE) {
+				viewHolder.complexLayout.setVisibility(View.GONE);
+			} else {
+				viewHolder.complexLayout.setVisibility(View.VISIBLE);
+			}
+			viewHolder.avatar = (FrameLayout) view.findViewById(R.id.flAvatar);
 			viewHolder.avatarImage = (ImageView) view
 					.findViewById(R.id.ivHomeListItemAvatar);
 			viewHolder.userName = (TextView) view
 					.findViewById(R.id.tvHomeListItemName);
-			viewHolder.mState = (TextView) view
-					.findViewById(R.id.tvHomeListItemState);
+			viewHolder.action = (TextView) view
+					.findViewById(R.id.tvHomeListIteAction);
 			viewHolder.itemTitle = (TextView) view
 					.findViewById(R.id.tvHomeListItemTitle);
 			viewHolder.bestAnswer = (TextView) view
@@ -47,25 +66,36 @@ public class HomePageAdapter extends ArrayAdapter<HomePageItemModel> {
 		} else {
 			view = convertView;
 			viewHolder = (ViewHolder) view.getTag();
+			if (itemModel.getLayoutType() == HomePageItemModel.LAYOUT_TYPE_SIMPLE) {
+				viewHolder.complexLayout.setVisibility(View.GONE);
+			} else {
+				viewHolder.complexLayout.setVisibility(View.VISIBLE);
+			}
 		}
-		viewHolder.avatarImage.setImageResource(itemModel.getAvatarImageId());
+		viewHolder.agreeCount.setText(itemModel.getAgreeCount() + " ");
+		viewHolder.action.setText(itemModel.getAction());
 		viewHolder.userName.setText(itemModel.getUserName());
-		viewHolder.mState.setText(itemModel.getmState());
 		viewHolder.itemTitle.setText(itemModel.getItemTitle());
-		viewHolder.agreeCount.setText(itemModel.getAgreeCount());
 		viewHolder.bestAnswer.setText(itemModel.getBestAnswer());
-		// 根据数据类型决定是否隐藏布局的下部分
-		if (itemModel.getLayoutType() == HomePageItemModel.LAYOUT_TYPE_SIMPLE) {
-			viewHolder.complexLayout.setVisibility(View.GONE);
-		}
+		// 对头像的监听，点击头像跳转到相对应的用户信息详情页
+		viewHolder.avatar.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				UserInfoActivity.actionStar(context,
+						Integer.toString(itemModel.getUserUid()));
+			}
+		});
 		return view;
 	}
 
 	class ViewHolder {
 		LinearLayout complexLayout;
+		FrameLayout avatar;
 		ImageView avatarImage;
 		TextView userName;
-		TextView mState;
+		TextView action;
 		TextView itemTitle;
 		TextView bestAnswer;
 		TextView agreeCount;
